1f3d0bc1ea90b0b33da299c0f4f11c87d4581660
Gabriel Peralta <prates_peralta@hotmail.com>
2022-05-05 21:20:59 -0300

    some changes

--- appointments/src/App.test.js
+++ appointments/src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, within } from '@testing-library/react';
+// import { AppointmentsPage } from './pages/AppointmentsPage';
+// import App from './App';
+// const ActualAppointmentsPage = jest.requireActual('clock');
+// jest.mock('appointmentsPage', () =>
+//   <div data-testid="appointmentsPage">
+//     <ActualAppointmentsPage />
+//   </div>
+// );
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   const { getAllByTestId, getByTestId } = render(<App />);
+//   const appointmentsPage = getByTestId('appointmentsPage');
+//   const appointmentsInApp = within(appointmentsPage).getAllByTestId('appointmentsPage');
+//   expect(appointmentsInApp.length).toBe(1);
+// });
